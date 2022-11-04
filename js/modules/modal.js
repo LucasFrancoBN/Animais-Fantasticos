93a18f1c7f4e8aa16ca881edc25413320cf3f243
Lucas <lucasfbnavarro@gmail.com>
2022-11-04 08:46:30 -0300

    remocao de pasta e mudanca no modal

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,4 +1,4 @@
-export default class initModal {
+export default class Modal {
   constructor(btnAbrir, btnFechar, containerModal) {
     this.btnAbrir = document.querySelector(btnAbrir);
     this.btnFechar = document.querySelector(btnFechar);
