ebb82a2a00aced812625eac0bc275f7e3be4f288
Lucas <lucasfbnavarro@gmail.com>
2022-11-04 15:47:47 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollAnima from "./modules/scroll-anima.js";
 import DropDownMenu from "./modules/dropdown.js";
 import MenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 
 const scrollSuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
 scrollSuave.init();
@@ -41,6 +41,8 @@ dropdown.init();
 const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
 menuMobile.init();
 
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+funcionamento.init();
+
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
-initFuncionamento();
